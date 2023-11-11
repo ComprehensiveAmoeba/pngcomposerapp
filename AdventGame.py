f301dc5d6772d36f26580b0f3ef3a1469e5a215a
ComprehensiveAmoeba <106018103+ComprehensiveAmoeba@users.noreply.github.com>
2023-11-12 00:10:49 +0100

    Update AdventGame.py
    chukwa app

--- AdventGame.py
+++ AdventGame.py
@@ -1,109 +1,81 @@
-import os
 import streamlit as st
 from PIL import Image, UnidentifiedImageError
 import io
 
-# Define constants
+# Constants
 MAX_IMAGES = 24
 IMAGE_WIDTH = 4
 IMAGE_HEIGHT = 6
+FRAME_OPTIONS = {"chukwa": 2, "chak k’an": 1}
+CORRECT_CHOICES = {
+    8: "chak k’an",
+    10: "chak k’an",
+    15: "chak k’an",
+    16: "chak k’an",
+    24: "chak k’an"
+}
 
-def validate_and_extract_frames(uploaded_files, pass_codes):
-    # This will hold the extracted frames
-    frames = []
+def extract_frame(uploaded_file, frame_number):
+    try:
+        gif = Image.open(uploaded_file)
+        if frame_number < gif.n_frames:
+            gif.seek(frame_number)
+            return gif.copy()
+        else:
+            return None
+    except UnidentifiedImageError:
+        return None
 
-    for i, uploaded_file in enumerate(uploaded_files):
-        try:
-            # Open the uploaded image file
-            gif = Image.open(uploaded_file)
+def create_final_image(uploaded_files, positions, frame_choices):
+    frame_size = Image.open(uploaded_files[0]).size
+    final_img = Image.new('RGBA', (IMAGE_WIDTH * frame_size[0], IMAGE_HEIGHT * frame_size[1]))
 
-            # Validate pass code
-            if pass_codes[i] < gif.n_frames:
-                gif.seek(pass_codes[i])
-                frame = gif.copy()
-                frames.append(frame)
-            else:
-                frames.append(None)
-
-        except UnidentifiedImageError:
-            frames.append(None)
-
-    return frames
-
-def create_final_image(frames):
-    # Create an empty image with the correct dimensions
-    final_img = Image.new('RGBA', (IMAGE_WIDTH * frames[0].width, IMAGE_HEIGHT * frames[0].height))
-
-    # Populate the final image with the frames
-    for i, frame in enumerate(frames):
+    for file, pos, choice in zip(uploaded_files, positions, frame_choices):
+        frame = extract_frame(file, FRAME_OPTIONS[choice])
         if frame:
-            x = (i % IMAGE_WIDTH) * frame.width
-            y = (i // IMAGE_WIDTH) * frame.height
+            x = ((pos - 1) % IMAGE_WIDTH) * frame.width
+            y = ((pos - 1) // IMAGE_WIDTH) * frame.height
             final_img.paste(frame, (x, y))
 
     return final_img
 
+def check_win_conditions(positions, frame_choices):
+    for pos, choice in zip(positions, frame_choices):
+        correct_choice = CORRECT_CHOICES.get(pos, "chukwa")
+        if choice != correct_choice:
+            return False
+    return True
+
 def main():
-    # Set page config to widen the app and set the title and favicon
-    st.set_page_config(page_title="Ancient Mosaic Lobby", layout="wide")
+    st.title("GIF Frame Composer")
 
-    st.title("The Workshop")
-
-    st.write("""
-    Valiant seekers, use the unearthed mosaic shard to restore the Ancient Mosaic.
-    
-    Heed these sacred tasks:
-
-    * Upload the Artefact: Select a file that forms part of our grand mosaic.
-    * Conceal Your Codex: Submit a unique secret code alongside your artefact.
-    * Form the Mosaic & Align the Codes: Arrange your artefact and enter your code. Only through perfect harmony will the deeper secrets be sensed.
-    * Claim the Triumph: Assemble the mosaic and the codes in their correct sequence to unlock a critical clue.
-    * Unveil the Gateway: If the codes resonate in their destined order, a pivotal hint will be bestowed, pointing the way to the secret chamber's password.
-
-    Embark with wisdom and valor!
-    """)
-
-    # File uploader allows user to add up to 24 GIFs
-    uploaded_files = st.file_uploader("Upload GIFs", type="gif", accept_multiple_files=True)
+    uploaded_files = st.file_uploader("Upload GIFs", type="gif", accept_multiple_files=True, max_uploads=MAX_IMAGES)
 
     if len(uploaded_files) > 0:
-        # Prompt for pass codes
-        pass_codes = []
-        for i, uploaded_file in enumerate(uploaded_files):
-            file_name = uploaded_file.name
-            pass_code = st.text_input(f"Enter Pass Code for {file_name} (GIF {i+1})", key=i)
-            try:
-                pass_codes.append(int(pass_code))
-            except ValueError:
-                st.error(f"Invalid Pass Code for {file_name} (GIF {i+1}). Please enter a number.")
-                return
+        positions = [st.number_input(f"Position for GIF {i+1} (1-24)", min_value=1, max_value=24, key=f"pos_{i}") for i in range(len(uploaded_files))]
+        frame_choices = [st.radio(f"Frame Choice for GIF {i+1}", options=list(FRAME_OPTIONS.keys()), key=f"choice_{i}") for i in range(len(uploaded_files))]
 
-        # Process the GIFs
-        frames = validate_and_extract_frames(uploaded_files, pass_codes)
+        if st.button("Create Mosaic"):
+            final_image = create_final_image(uploaded_files, positions, frame_choices)
+            st.image(final_image)
 
-        # Check if all passcodes are valid
-        if None not in frames and len(frames) == MAX_IMAGES:
-            st.success(os.getenv("GAME_SUCCESS_MESSAGE", "Success message not set"))
-        else:
-            st.warning(os.getenv("GAME_ALTERNATIVE_MESSAGE", "Alternative message not set"))
+            # Check win conditions
+            if check_win_conditions(positions, frame_choices):
+                st.success("Choose one of the options posted in our slack channel")
+            else:
+                st.warning("Try harder to unveil the decisive hint")
 
-        # Create the final image
-        final_image = create_final_image(frames)
+            # Save the final image to a buffer
+            buf = io.BytesIO()
+            final_image.save(buf, format="PNG")
 
-        # Display the final image
-        st.image(final_image)
-
-        # Save the final image to a buffer
-        buf = io.BytesIO()
-        final_image.save(buf, format="PNG")
-
-        # Provide a download link to the final image
-        st.download_button(
-            label="Download the final image",
-            data=buf.getvalue(),
-            file_name="final_image.png",
-            mime="image/png"
-        )
+            # Provide a download link to the final image
+            st.download_button(
+                label="Download the final image",
+                data=buf.getvalue(),
+                file_name="final_image.png",
+                mime="image/png"
+            )
 
 if __name__ == "__main__":
     main()
