d021699beacbacb8c52a2f15dbade1af549c5e31
ComprehensiveAmoeba <106018103+ComprehensiveAmoeba@users.noreply.github.com>
2023-11-29 17:35:35 +0100

    Update AdventGame.py

--- AdventGame.py
+++ AdventGame.py
@@ -33,6 +33,7 @@ import io
 
 
 
+
 # Constants
 MAX_IMAGES = 24
 IMAGE_WIDTH = 4
@@ -58,23 +59,23 @@ def extract_frame(uploaded_file, frame_number):
     except UnidentifiedImageError:
         return None
 
-def create_final_image(uploaded_files, positions, frame_choices):
-    frame_size = Image.open(uploaded_files[0]).size
+def create_final_image(uploaded_files_data):
+    frame_size = Image.open(uploaded_files_data[0]['file']).size
     final_img = Image.new('RGBA', (IMAGE_WIDTH * frame_size[0], IMAGE_HEIGHT * frame_size[1]))
 
-    for file, pos, choice in zip(uploaded_files, positions, frame_choices):
-        frame = extract_frame(file, FRAME_OPTIONS[choice])
+    for data in uploaded_files_data:
+        frame = extract_frame(data['file'], FRAME_OPTIONS[data['frame_choice']])
         if frame:
-            x = ((pos - 1) % IMAGE_WIDTH) * frame.width
-            y = ((pos - 1) // IMAGE_WIDTH) * frame.height
+            x = ((data['position'] - 1) % IMAGE_WIDTH) * frame.width
+            y = ((data['position'] - 1) // IMAGE_WIDTH) * frame.height
             final_img.paste(frame, (x, y))
 
     return final_img
 
-def check_win_conditions(positions, frame_choices):
-    for pos, choice in zip(positions, frame_choices):
-        correct_choice = CORRECT_CHOICES.get(pos, "chukwa")
-        if choice != correct_choice:
+def check_win_conditions(uploaded_files_data):
+    for data in uploaded_files_data:
+        correct_choice = CORRECT_CHOICES.get(data['position'], "chukwa")
+        if FRAME_OPTIONS[data['frame_choice']] != FRAME_OPTIONS[correct_choice]:
             return False
     return True
 
@@ -83,39 +84,26 @@ def main():
 
     uploaded_files = st.file_uploader("Upload mosaic shards", type="gif", accept_multiple_files=True)
 
-    positions = []
-    frame_choices = []
+    uploaded_files_data = []
 
     if len(uploaded_files) > 0:
         for i, uploaded_file in enumerate(uploaded_files):
             file_label = uploaded_file.name
-            unique_key = f"{file_label}_{i}"  # Using file name and index for unique key
+            unique_key = f"{file_label}_{i}"
             col1, col2 = st.columns([3, 1])
             with col1:
-                pos_label = f"{file_label} - Position (1-24):"
-                positions.append(st.number_input(pos_label, min_value=1, max_value=24, key=f"pos_{unique_key}"))
+                position = st.number_input(f"{file_label} - Position (1-24):", min_value=1, max_value=24, key=f"pos_{unique_key}")
             with col2:
-                frame_choice_label = f""
-                frame_choices.append(st.radio(frame_choice_label, options=list(FRAME_OPTIONS.keys()), key=f"choice_{unique_key}", horizontal=True))
+                frame_choice = st.radio("", options=list(FRAME_OPTIONS.keys()), key=f"choice_{unique_key}", horizontal=True)
+            uploaded_files_data.append({'file': uploaded_file, 'position': position, 'frame_choice': frame_choice})
 
     if st.button("Craft the Mosaic"):
         if len(uploaded_files) == MAX_IMAGES:
-            final_image = create_final_image(uploaded_files, positions, frame_choices)
-            st.image(final_image)
-
-            if check_win_conditions(positions, frame_choices):
+            if check_win_conditions(uploaded_files_data):
+                final_image = create_final_image(uploaded_files_data)
+                st.image(final_image)
                 st.success("As the hidden message of the ancient mosaic unites...")
-                # Save the final image to a buffer
-                buf = io.BytesIO()
-                final_image.save(buf, format="PNG")
-
-                # Provide a download link to the final image
-                st.download_button(
-                    label="Download the full mosaic",
-                    data=buf.getvalue(),
-                    file_name="final_image.png",
-                    mime="image/png"
-                )
+                # Additional success actions
             else:
                 st.error("The mosaic pieces are not in the correct order. Try again.")
         else:
